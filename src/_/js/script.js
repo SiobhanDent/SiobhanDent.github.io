c5124d70a7fa2014c81389003b0ff9d216978c9c
Andrew Gleeson <heygleeson@gmail.com>
2024-05-04 16:15:51 +1000

    Updated light/dark toggle icon

--- src/_/js/script.js
+++ src/_/js/script.js
@@ -14,7 +14,11 @@ function setTheme(theme) {
 	document.documentElement.dataset.theme = theme;
 	// Update Icons/Emblems
 	if (theme == 'light') {
+		lightIcon.classList.remove("fa-sun");
+		lightIcon.classList.add("fa-moon");
 	} else {
+		lightIcon.classList.remove("fa-moon");
+		lightIcon.classList.add("fa-sun");
 	}
 }
 
