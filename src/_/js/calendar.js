ec19b4b08da4183c13a915167689650f8b957fcd
Andrew Gleeson <heygleeson@gmail.com>
2024-05-07 02:14:50 +1000

    Updated calendar to table, added highlight to current day

--- src/_/js/calendar.js
+++ src/_/js/calendar.js
@@ -8,6 +8,11 @@ const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep
 function renderCalendar() {
 	const title = document.getElementById('calendar-title');
 	title.innerText = `${monthNames[currentMonth]} ${currentYear}`;
+
+	// Highlight cell of current day.
+	let currentDay = new Date().getDate();
+	let days = document.getElementsByClassName("day-active");
+	days[currentDay - 1].classList.add('day-current');
 }
 
 window.addEventListener('load', (e) => {
