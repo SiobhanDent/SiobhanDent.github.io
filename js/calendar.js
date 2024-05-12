1fe9920d89f7d9ffaf919bbbc693a175dac89ab3
haraiva <haraiva@users.noreply.github.com>
2024-05-12 14:55:51 +0000

    deploy: b157e9e0f88cd5ec3a12f250d04a0e2373c42d87

--- js/calendar.js
+++ js/calendar.js
@@ -2,7 +2,13 @@ let date = new Date();
 let currentYear = date.getFullYear();
 let currentMonth = date.getMonth();
 
+// Constants
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 function renderCalendar() {
+	const title = document.getElementById('calendar-title');
+	title.innerText = `${monthNames[currentMonth]} ${currentYear}`;
+
 	// Highlight cell of current day.
 	let currentDay = new Date().getDate();
 	let days = document.getElementsByClassName("day-active");
